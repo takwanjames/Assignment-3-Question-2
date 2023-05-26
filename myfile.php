7132be1babcdf65707b7ebb2b0679872e7255cca
takwanjames <133966388+takwanjames@users.noreply.github.com>
2023-05-26 10:44:23 +0100

    Create myfile.php

---
+++ myfile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Information</title>
+</head>
+<body>
+
+<?php
+// Student Information
+$name = "TAKWAN JAMES TAKWAN";
+$registrationNumber = "2021/HND/33019/CS";
+$stateOfOrigin = "Cross River";
+$projectTopic = " Design and Implementation of Computerized Hostel Allocation System: A study of PolyUnwana Hostel";
+
+// Output the data 
+echo "<h1>Student Data</h1>";
+echo "<p><strong>Name:</strong> " . $name . "</p>";
+echo "<p><strong>Registration Number:</strong> " . $registrationNumber . "</p>";
+echo "<p><strong>State of Origin:</strong> " . $stateOfOrigin . "</p>";
+echo "<p><strong>Project Topic:</strong> " . $projectTopic . "</p>";
+?>
+
+</body>
+</html>
